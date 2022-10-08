6b5ab7de53120dc025563d9986ac29da4d6caf7d
JakobBollman <jakob@bollman.ca>
2022-10-08 10:56:38 -0600

    submit

--- vampire.js
+++ vampire.js
@@ -10,22 +10,34 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
-  get numberOfVampiresFromOriginal() {
-
+  get numberOfVampiresFromOriginal(){
+    let next = this;
+    for(let i = 0; i < 20; i++){
+      if(next.creator === null){
+        return i;
+      } else {
+        next = next.creator;
+      }
+    }
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    if(this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal){
+      return true;
+    } else {
+      return false;
+    }
   }
 
   /** Stretch **/
